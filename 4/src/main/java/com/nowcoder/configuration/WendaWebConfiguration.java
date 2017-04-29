e92b55a54f5c547256854e7aef69b21db3890218
DreamOfFootPrints <2822850867@qq.com>
2017-04-29 10:31:37 +0800

    第4次课
    密码MD5，token，拦截器

---
+++ 4/src/main/java/com/nowcoder/configuration/WendaWebConfiguration.java
@@ -0,0 +1,29 @@
+package com.nowcoder.configuration;
+
+import com.nowcoder.interceptor.LoginRequiredInterceptor;
+import com.nowcoder.interceptor.PassportInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by nowcoder on 2016/7/3.
+ */
+
+//拦截器，进行链路回调
+@Component
+public class WendaWebConfiguration extends WebMvcConfigurerAdapter {
+    @Autowired
+    PassportInterceptor passportInterceptor;
+
+    @Autowired
+    LoginRequiredInterceptor loginRequiredInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(passportInterceptor);
+        registry.addInterceptor(loginRequiredInterceptor).addPathPatterns("/user/*");
+        super.addInterceptors(registry);
+    }
+}
