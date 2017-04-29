89a0646fa546ed3d8e7a7af87f35e7f128dcf959
DreamOfFootPrints <2822850867@qq.com>
2017-04-29 09:59:52 +0800

    第二次课
    简单的介绍

---
+++ 2/src/main/java/com/nowcoder/WendaApplication.java
@@ -0,0 +1,14 @@
+package com.nowcoder;
+
+import com.nowcoder.model.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WendaApplication {
+//主要是基础的东西的学习
+
+	public static void main(String[] args) {
+		SpringApplication.run(WendaApplication.class, args);
+	}
+}
